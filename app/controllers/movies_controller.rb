1f47236a5ea4720e2dc2d8991523a967a702b000
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-14 09:01:31 -0400

    Completed Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,17 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
-  private
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
+private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
